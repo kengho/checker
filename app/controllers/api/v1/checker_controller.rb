299c0c491148ea1aed448bc5ad4004709336e4a2
Alexander Morozov <ntcomp12@gmail.com>
2016-04-20 04:12:47 -0400

    changed api

--- app/controllers/api/v1/checker_controller.rb
+++ app/controllers/api/v1/checker_controller.rb
@@ -7,15 +7,21 @@ class Api::V1::CheckerController < Api::V1::BaseController
     if ip =~ /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/
       check_request = Net::Ping::TCP.new(ip, 5100)
       availability = check_request.ping?
-      render json: { "availability" => availability }
+      render json: { "data" => { "availability" => availability } }
     else
-      render nothing: true, status: 400, content_type: "text/html"
+      render json: { "errors" => [{ title: "external", detail: "Expected ip like /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/ as param" }] }
     end
   end
 
   private
     def authenticate
       token = ENV["CHECKER_TOKEN"]
+      unless token
+        render json: { "errors" => [{ title: "internal", detail: "Internal error 01" }] } and return
+      end
+      unless params[:token]
+        render json: { "errors" => [{ title: "external", detail: "Expected token as param" }] } and return
+      end
       if !ActiveSupport::SecurityUtils.secure_compare(params[:token], token)
          render nothing: true, status: 401, content_type: "text/html"
       end
