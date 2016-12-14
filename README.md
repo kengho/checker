267dcffd39db5e2c56c6759a855f06545ae0c20a
Alexander Morozov <ntcomp12@gmail.com>
2016-12-14 08:53:41 -0500

    moved to api with specifying port and removed vipnet mentions

--- README.md
+++ README.md
@@ -1,15 +1,33 @@
-# ViPNet™ Checker
+# Checker
 
 ## Summary
 
-Provides API for checking ViPNet™ nodes' availability. May be used with with [vipnet_interface-rails](https://github.com/kengho/vipnet_interface-rails).
+Provides API for making TCP pings via ["net/ping"](https://github.com/djberg96/net-ping).
 
-# Installing
+## Installing
 
 [rvm + passenger + nginx](https://www.phusionpassenger.com/library/walkthroughs/deploy/ruby/ownserver/nginx/oss/install_language_runtime.html/).
 
+## Usage
+
+Setup `.env`
+
+```
+CHECKER_TOKEN=TOKEN
+```
+
+and you will able to make GET requests:
+
+```
+https://checker.my.domain/api/v1/check/?ip=192.0.2.1&port=80&token=TOKEN
+```
+
+or
+
+```
+https://checker.my.domain/?ip=192.0.2.1&port=80&token=TOKEN
+```
+
 ## License
 
-ViPNet™ Checker is distributed under the WTFPL.
-
-ViPNet™ is registered trademark of InfoTeCS Gmbh, Russia.
+Checker is distributed under the WTFPL.
--- app/controllers/api/v1/checker_controller.rb
+++ app/controllers/api/v1/checker_controller.rb
@@ -4,26 +4,60 @@ class Api::V1::CheckerController < Api::V1::BaseController
   def check
     require "net/ping"
     ip = params[:ip]
-    if ip =~ /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/
-      check_request = Net::Ping::TCP.new(ip, 5100)
-      availability = check_request.ping?
-      render json: { "data" => { "availability" => availability } }
-    else
-      render json: { "errors" => [{ "title" => "external", "detail" => "Expected ip like /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/ as param" }] }
+    port = params[:port]
+
+    ip_regexp = /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/
+    unless ip =~ ip_regexp
+      render json: {
+        "errors" => [{
+          "title" => "external",
+          "detail" => "Expected param 'ip' like /#{ip_regexp.source}/",
+        }],
+      }
+      return
     end
+    port_regexp = /^\d+$/
+    unless port =~ port_regexp && port.to_i >= 1 && port.to_i <= 65535
+      render json: {
+        "errors" => [{
+          "title" => "external",
+          "detail" => "Expected param 'port' from 1 to 65535",
+        }],
+      }
+      return
+    end
+
+    check_request = Net::Ping::TCP.new(ip, port)
+    availability = check_request.ping?
+    render json: { "data" => { "availability" => availability } } and return
   end
 
   private
     def authenticate
-      token = ENV["CHECKER_TOKEN"]
-      unless token
-        render json: { "errors" => [{ "title" => "internal", "detail" => "Internal error 01" }] } and return
+      local_token = ENV["CHECKER_TOKEN"]
+      unless local_token
+        render json: {
+          "errors" => [{
+            "title" => "internal",
+            "detail" => "Internal error 01",
+          }],
+        }
+        return
       end
-      unless params[:token]
-        render json: { "errors" => [{ "title" => "external", "detail" => "Expected token as param" }] } and return
+
+      remote_token = params[:token]
+      unless remote_token
+        render json: {
+          "errors" => [{
+            "title" => "external",
+            "detail" => "Expected param 'token'",
+          }],
+        }
+        return
       end
-      if !ActiveSupport::SecurityUtils.secure_compare(params[:token], token)
-        render nothing: true, status: :unauthorized, content_type: "text/html"
+
+      if !ActiveSupport::SecurityUtils.secure_compare(remote_token, local_token)
+        render body: nil, status: :unauthorized, content_type: "text/html" and return
       end
     end
 end
